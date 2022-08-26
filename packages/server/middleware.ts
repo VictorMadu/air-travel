b40ffc003c83c1ff60214c95a532b0fde33503eb
VictorMadu <ebubevm@gmail.com>
2022-08-26 05:18:33 +0100

    feat: Add cors

--- packages/server/middleware.ts
+++ packages/server/middleware.ts
@@ -1,6 +1,8 @@
 import express, { Express } from "express";
+import cors from "cors";
 
 export default function attachMiddleware(app: Express) {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+    app.use(cors());
 }
